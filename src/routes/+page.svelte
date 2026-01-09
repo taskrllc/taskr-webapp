4e34d59e5495f545e2153850ab39f10d139bb1d9
XnLogicaL <acceltemen@gmail.com>
2026-01-09 19:32:23 +0300

    feat(frontend): light mode default

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
- import logo from "$lib/assets/logo-transparent.png"
+    import logo from "$lib/assets/logo-black.png";
 </script>
 
 <div class="hero bg-base-200 min-h-screen">
-  <div class="hero-content flex-col lg:flex-row">
-    <img src={logo} alt="" />
-    <div>
-      <h1 class="text-5xl font-bold">taskr</h1>
-      <p class="py-6">
-        Welcome to Taskr, a platform where you can clean and
-        get money and stuff. Very good and fun! 10/10
-      </p>
-      <button class="btn btn-primary">Get Started</button>
+    <div class="hero-content flex-col lg:flex-row">
+        <img src={logo} alt="" />
+        <div>
+            <h1 class="text-5xl font-bold">taskr</h1>
+            <p class="py-6">
+                Welcome to Taskr, a platform where you can clean and get money
+                and stuff. Very good and fun! 10/10
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+        </div>
     </div>
-  </div>
 </div>
