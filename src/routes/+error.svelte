a53b5dbe259d4c4c05f8fde15e408fabc49bdeba
XnLogicaL <acceltemen@gmail.com>
2026-01-09 20:37:06 +0300

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,12 @@
+<script lang="ts">
+    import { page } from "$app/state";
+</script>
+
 <div class="hero bg-base-200 min-h-screen">
     <div class="hero-content text-center">
         <div class="max-w-md">
-            <h1 class="text-5xl font-bold">404</h1>
-            <p class="py-6">Could not find the page you're looking for :(</p>
+            <h1 class="text-5xl font-bold">Oops!</h1>
+            <p class="py-6"><b>{page.status}</b> - {page.error?.message}</p>
             <a href="/" class="btn btn-primary"
                 >&LeftArrow; &MediumSpace; Home</a
             >
